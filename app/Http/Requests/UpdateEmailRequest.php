f4b2cccd12075dae39ac28e316d09f366583b66c
ushimaru <uxsxmxr14@gmail.com>
2021-05-17 19:57:34 +0900

    config auth.php変更4

--- app/Http/Requests/UpdateEmailRequest.php
+++ app/Http/Requests/UpdateEmailRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 
 class UpdateEmailRequest extends FormRequest
