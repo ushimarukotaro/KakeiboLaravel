7a311e091cebff4879971af1b18e9b3992099305
ushimaru <uxsxmxr14@gmail.com>
2021-05-17 17:15:18 +0900

    config auth.php変更

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,8 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => 'User',
+            // 'model' => App\Models\User::class,
         ],
 
         // 'users' => [
